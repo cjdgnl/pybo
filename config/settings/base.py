abe18bba9d4c2c9ab51e0a76d40f8945edb79b8c
cjdgnl <dusvlf1021@naver.com>
2024-10-28 15:26:54 +0900

    Settings 변경

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,6 +124,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-
 # 로그인 성공후 이동하는 URL
 LOGIN_REDIRECT_URL = '/'
